ed51fe6ff8e33516ae2bebdef1002953e8c6c6f3
Asen Tahchiyski <asen.andreev.tahchiyski@puzzel.com>
2018-11-16 14:25:08 +0200

    Add new message and create message events

---
+++ public/js/index.js
@@ -0,0 +1,17 @@
+const socket = io();
+socket.on('connect', function () {
+  console.log('Connected to server');
+
+  socket.emit('createMessage', {
+    from: 'Gosho',
+    text: 'Ot koq?'
+  });
+});
+
+socket.on('disconnect', function () {
+  console.log('Disconnected from server');
+});
+
+socket.on('newMessage', function (message) {
+  console.log('newMessage', message);
+});
